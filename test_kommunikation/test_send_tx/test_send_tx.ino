fae679603359741891a9c5dcba239e06e9ec26af
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-05 19:22:57 +0200

    Begun Testing duplex communication

--- test_kommunikation/test_send_tx/test_send_tx.ino
+++ test_kommunikation/test_send_tx/test_send_tx.ino
@@ -6,6 +6,8 @@ const byte txPin = 1;
 // set up a new serial object
 SoftwareSerial serialObject = SoftwareSerial(rxPin, txPin);
 int index;
+String inputString = "";
+boolean stringComplete = false;
 
 void setup() {
   pinMode(rxPin, INPUT);
@@ -13,10 +15,29 @@ void setup() {
   // Set the BAUD rate for the serial communication
   serialObject.begin(9600);
   index = 0;
+  inputString.reserve(200);
 }
 
 void loop() {
-  serialObject.println(index);
-  index++;
-  delay(1000);
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    serialObject.println(inputString);
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (serialObject.available()) {
+    // get the new byte:
+    char inChar = (char)serialObject.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
