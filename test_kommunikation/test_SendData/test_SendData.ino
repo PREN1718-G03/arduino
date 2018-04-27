b3dd6afc673cfe839f5de598880554d35c27691e
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-27 17:47:34 +0200

    Protokoll für Due implementiert

--- test_kommunikation/test_SendData/test_SendData.ino
+++ test_kommunikation/test_SendData/test_SendData.ino
@@ -19,39 +19,65 @@ void setup_arduino_due() {
 }
 
 void loop() {
+  delay(500);
 }
 
 void sendData_uno() {
   if (!sentCommand) {
-  	// Flush the read queue
-  	while (Serial.available > 0) {
-  	    Serial.read();
-  	}
+    // Flush the read queue
+    while (Serial.available > 0) {
+        Serial.read();
+    }
     Serial.println("SendData");
     sentCommand = true;
   }
   if (Serial.available() > 0) {
-  	height = Serial.read() * 256 + Serial.read();
-  	distanceToPillar = Serial.read() * 256 + Serial.read();
-  	distanceToTarget = Serial.read() * 256 + Serial.read();
-  	while (Serial.available() > 0){
-  		incomingByte = Serial.read();
-  		if (incomingByte = '\n') {
-  			receivedData = true;
-  		}
-  	}
+    height = Serial.read() * 256 + Serial.read();
+    distanceToPillar = Serial.read() * 256 + Serial.read();
+    distanceToTarget = Serial.read() * 256 + Serial.read();
+    while (Serial.available() > 0){
+      incomingByte = Serial.read();
+      if (incomingByte = '\n') {
+        receivedData = true;
+      }
+    }
   }
   if (receivedData) {
-  	Serial.print("Height: ");
-  	Serial.print(height, DEC);
-  	Serial.print("Distance to pillar: ");
-  	Serial.print(distanceToPillar, DEC);
-  	Serial.print("Distance to target: ");
-  	Serial.println(distanceToTarget, DEC);
+    Serial.print("Height: ");
+    Serial.print(height, DEC);
+    Serial.print("Distance to pillar: ");
+    Serial.print(distanceToPillar, DEC);
+    Serial.print("Distance to target: ");
+    Serial.println(distanceToTarget, DEC);
   }
-  delay(500);
 }
 
 void sendData_due() {
-
+    if (!sentCommand) {
+    // Flush the read queue
+    while (Serial1.available > 0) {
+        Serial1.read();
+    }
+    Serial1.println("SendData");
+    sentCommand = true;
+  }
+  if (Serial1.available() > 0) {
+    height = Serial1.read() * 256 + Serial1.read();
+    distanceToPillar = Serial1.read() * 256 + Serial1.read();
+    distanceToTarget = Serial1.read() * 256 + Serial1.read();
+    while (Serial1.available() > 0){
+      incomingByte = Serial1.read();
+      if (incomingByte = '\n') {
+        receivedData = true;
+      }
+    }
+  }
+  if (receivedData) {
+    Serial.print("Height: ");
+    Serial.print(height, DEC);
+    Serial.print("Distance to pillar: ");
+    Serial.print(distanceToPillar, DEC);
+    Serial.print("Distance to target: ");
+    Serial.println(distanceToTarget, DEC);
+  }
 }
