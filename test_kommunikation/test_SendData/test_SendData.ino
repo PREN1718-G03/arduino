5a205e29fa46e006d674f747a59e624cca605ed3
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-27 17:25:35 +0200

    Protokoll für Uno implementiert

--- test_kommunikation/test_SendData/test_SendData.ino
+++ test_kommunikation/test_SendData/test_SendData.ino
@@ -1,13 +1,19 @@
 bool sentCommand = false;
+bool receivedData = false;
+char incomingByte = 0;
+int height = 0;
+int distanceToPillar = 0;
+int distanceToTarget = 0;
+
 
 void setup() {
 }
 
-void setup_arduino_uno(){
+void setup_arduino_uno() {
   Serial.begin(9600);
 }
 
-void setup_arduino_due(){
+void setup_arduino_due() {
   Serial.begin(9600);
   Serial1.begin(9600);
 }
@@ -15,12 +21,37 @@ void setup_arduino_due(){
 void loop() {
 }
 
-void sendData_uno(){
+void sendData_uno() {
   if (!sentCommand) {
+  	// Flush the read queue
+  	while (Serial.available > 0) {
+  	    Serial.read();
+  	}
     Serial.println("SendData");
     sentCommand = true;
   }
-  while (Serial.available() > 0) {
-    
+  if (Serial.available() > 0) {
+  	height = Serial.read() * 256 + Serial.read();
+  	distanceToPillar = Serial.read() * 256 + Serial.read();
+  	distanceToTarget = Serial.read() * 256 + Serial.read();
+  	while (Serial.available() > 0){
+  		incomingByte = Serial.read();
+  		if (incomingByte = '\n') {
+  			receivedData = true;
+  		}
+  	}
   }
+  if (receivedData) {
+  	Serial.print("Height: ");
+  	Serial.print(height, DEC);
+  	Serial.print("Distance to pillar: ");
+  	Serial.print(distanceToPillar, DEC);
+  	Serial.print("Distance to target: ");
+  	Serial.println(distanceToTarget, DEC);
+  }
+  delay(500);
+}
+
+void sendData_due() {
+
 }
